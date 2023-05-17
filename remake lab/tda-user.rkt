7773ac0d36163beb9c4218ffdf661ccb0b9265fb
byroncaices <byron.caices@gmail.com>
2023-05-17 18:47:59 -0400

    v3.0.2
    rd

--- remake lab/tda-user.rkt
+++ remake lab/tda-user.rkt
@@ -2,3 +2,9 @@
 (provide (all-defined-out))
 
 (define user (lambda (name) name))
+
+
+(define filtro (lambda (x)
+                 (not(string-contains? x "hola"))))
+
+(define names (list "pehola" "alo" "lola" "hola" "papaholapapa"))
